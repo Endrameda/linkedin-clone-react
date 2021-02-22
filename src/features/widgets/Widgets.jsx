40ec745026e966345bf30594da362e6157234a72
Sardor <endrameda98@gmail.com>
2021-02-22 15:27:23 +0500

    deploy app

--- src/features/widgets/Widgets.jsx
+++ src/features/widgets/Widgets.jsx
@@ -2,10 +2,32 @@ import React from 'react';
 
 import './Widgets.scss'
 
+import InfoIcon from '@material-ui/icons/Info';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+
 const Widgets = () => {
+
+    const newsArticle = (heading, subtitle) => (
+        <div className="widgets__article">
+            <div className="widgets__articleLeft">
+                <FiberManualRecordIcon/>
+            </div>
+            <div className="widgets__articleRight">
+                <h4>{ heading }</h4>
+                <p> { subtitle } </p>
+            </div>
+        </div>
+    )
+
     return (
-        <div>
-            
+        <div className="widgets">
+            <div className="widgets__header">
+                <h2>
+                    Widgets
+                </h2>
+                <InfoIcon/>
+            </div>
+            { newsArticle('Sardor is back', 'Top news today!') }
         </div>
     );
 };
